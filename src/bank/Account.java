2f6e7e82a1473ed1e7a996755ab8047ad8fb496a
Tristan Perkins <trisperk@hotmail.com>
2017-12-30 09:55:14 +0300

    Added docs/comments to Player and Account

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -1,26 +1,62 @@
 package bank;
 
+/**
+ * A bank account for a player, stores the players money
+ * @author trisp
+ * @version 1.0
+ * 
+ * Changelog:
+ * 1.0 - Created base iteration with default value for starting money
+ */
+
 public class Account {
 	
+	//FIELDS
+	/**
+	 * Stores the players current money
+	 */
 	private int money;
 	
+	//CONSTRUCTOR
+	
+	/**
+	 * Creates the bank account with 1500
+	 */
 	public Account() {
 		//starting amount is 1500
 		money = 1500;
 	}
 	
+	//METHODS
+	
+	/**
+	 * Gets the current amount of money in the players account
+	 * @return The amount of money in the players account
+	 */
 	public int getMoney() {
 		return money;
 	}
 	
+	/**
+	 * Adds money to the players account
+	 * @param amount THe amount of money to add to the players account
+	 */
 	public void addMoney(int amount) {
 		money += amount;
 	}
 	
+	/**
+	 * Removes money from the players account
+	 * @param amount The amount of money to remove from the players account
+	 */
 	public void removeMoney(int amount) {
 		money -= amount;
 	}
 	
+	/**
+	 * Gets whether the account has less than 0 funds and is therefore bankrupt
+	 * @return true if account has less than 0 funds, otherwise false
+	 */
 	public boolean isBankrupt() {
 		if (money < 0) {
 			return true;
--- src/bank/Player.java
+++ src/bank/Player.java
@@ -1,31 +1,75 @@
 package bank;
 
+/**
+ * A PLayer object in the game, has a bank account
+ * @author trisp
+ * @version 1.0
+ *
+ * Changelog:
+ * 1.0 - Created first iteration of the player
+ */
+
 public class Player {
 	
+	//FIELDS
+	/**
+	 * Holds the players bank account
+	 */
 	private Account account;
+	/**
+	 * Holds the name of the player
+	 */
 	private String playerName;
 	
+	//CONSTRUCTOR
+	
+	/**
+	 * Creates the player with the given name and a new account
+	 * @param playerName
+	 */
 	public Player(String playerName) {
 		this.playerName = playerName;
 		account = new Account();
 	}
 	
+	//METHODS
+	
+	/**
+	 * Gets the name of the player
+	 * @return the players name
+	 */
 	public String getName() {
 		return playerName;
 	}
 	
+	/**
+	 * Adds money to this players account
+	 * @param amount the amount of money to add
+	 */
 	public void addMoneyToAccount(int amount) {
 		account.addMoney(amount);
 	}
 	
+	/**
+	 * Removes money from this players account
+	 * @param amount the amount of money to remove
+	 */
 	public void removeMoneyFromAccount(int amount) {
 		account.removeMoney(amount);
 	}
 	
+	/**
+	 * Gets the amount of money in this players account
+	 * @return the amount of money in the plaeyrs account
+	 */
 	public int getMoneyInAccount() {
 		return account.getMoney();
 	}
 	
+	/**
+	 * Gets whether the player is out of money
+	 * @return true if player has less than 0 money
+	 */
 	public boolean playerIsBankrupt() {
 		return account.isBankrupt();
 	}
